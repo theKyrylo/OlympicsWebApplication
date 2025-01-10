221922610b433563879dbf396fcfa3957b78ff0c
theKyrylo <zhulin.pers0n4l@gmail.com>
2025-01-10 17:08:36 +0100

    Initial commit

---
+++ OlympicsWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OlympicsWebApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
